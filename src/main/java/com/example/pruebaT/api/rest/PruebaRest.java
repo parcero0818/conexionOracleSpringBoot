a04bcf7885895eafcf410d0cebd49e0018ee5793
parcero0818 <john.sgv@outlook.com>
2018-05-15 01:51:19 -0500

    autenticacion del rest
    Signed-off-by: parcero0818 <john.sgv@outlook.com>

--- src/main/java/com/example/pruebaT/api/rest/PruebaRest.java
+++ src/main/java/com/example/pruebaT/api/rest/PruebaRest.java
@@ -3,6 +3,7 @@ package com.example.pruebaT.api.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class PruebaRest {
 		this.repository = repo;
 	}
 	
+	@CrossOrigin
 	@GetMapping
 	public List<PruebaModel> obtenerPruebas(){
 		return repository.findAll();
---
+++ src/main/java/com/example/pruebaT/api/security/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.pruebaT.api.security;
+
+import org.apache.tomcat.jni.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		String password = passwordEncoder().encode("admin");
+		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+
+		/*
+		 * Otra forma de hacerlo auth.inMemoryAuthentication()
+		 * .passwordEncoder(NoOpPasswordEncoder.getInstance())
+		 * .withUser("user").password("password").roles("USER");
+		 */
+
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+
+}
